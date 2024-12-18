4efb59686ece8aac0dd838698deae6946a9464d0
beunyeong <beunyeong.b@gmail.com>
2024-12-18 20:00:16 +0900

    🍻 test : 테스트 코드 추가

--- src/test/java/com/example/demo/entity/ItemJPQLTest.java
+++ src/test/java/com/example/demo/entity/ItemJPQLTest.java
@@ -1,4 +1,45 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
 class ItemJPQLTest {
-  
-}
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    @DisplayName("JPQL UPDATE를 이용하여 status를 null로 설정시 예외 발생 확인")
+    void updateStatusToNull_shouldNull() {
+        Item item = new Item();
+        item.setName("Test Item");
+        item.setDescription("Test Description");
+        item.setStatus("ACTIVE");
+
+        entityManager.persist(item);
+        entityManager.flush();      // 강제 반영
+
+        // 강제 null 적용
+        assertThatThrownBy(() -> {
+            int updatedCount = entityManager.createQuery(
+                            "UPDATE Item i SET i.status = null WHERE i.id = :id")
+                    .setParameter("id", item.getId())
+                    .executeUpdate();  //  쿼리 실행
+
+        }).isInstanceOf(PersistenceException.class);
+
+        }
+    }
+
--- src/test/java/com/example/demo/util/PasswordEncoderTest.java
+++ src/test/java/com/example/demo/util/PasswordEncoderTest.java
@@ -1,4 +1,29 @@
+package com.example.demo.util;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordEncoderTest {
-  
+
+    // 1. 암호화된 비민번호는 null이 아니여야한다.
+    @Test
+    void testEncode_NotNull() {
+        String rawPassword = "abc123";
+        String encodedPassword = PasswordEncoder.encode(rawPassword);
+
+        assertNotNull(encodedPassword, "암호화된 비밀번호는 null이 아니여야 합니다.");
+
+    }
+
+
+    // 2.사용자가 입력한 비밀번호와, 암호화된 비밀번호는 달라야 한다.
+    @Test
+    void testEncode_NotSameAsRawPassword() {
+        String rawPassword = "abc456";
+        String encodedPassword = PasswordEncoder.encode(rawPassword);
+
+        assertNotEquals(encodedPassword, rawPassword, "암호화된 비밀번호는 사용자가 입력한 비밀번호와 달라야 합니다.");
+    }
+
 }
